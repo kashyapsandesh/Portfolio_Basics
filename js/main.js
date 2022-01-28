b444c1f1c83abbb85347ef8020b57f69f1886e24
Kashyap Sandesh <sandeshghimire202@gmail.com>
2022-01-28 14:09:28 +0545

    Basic_portfolio

---
+++ js/main.js
@@ -0,0 +1,88 @@
+//portfolio Item Filter
+const filterContainer=document.querySelector(".portfolio-filter"),
+filterBtns=filterContainer.children,
+totalFilterBtn=filterBtns.length,
+portfolioItems=document.querySelectorAll(".portfolio-item"),
+totalPortfolioItem=portfolioItems.length;
+for(let i=0; i<totalFilterBtn; i++)
+{
+    filterBtns[i].addEventListener("click", function(){
+    filterContainer.querySelector(".active").classList.remove("active");
+    this.classList.add("active");
+         
+    const filterValue=this.getAttribute("data-filter");
+for(let k=0; k<totalPortfolioItem; k++)
+{
+if (filterValue === portfolioItems[k].getAttribute("data-category")){
+    portfolioItems[k].classList.remove("hide");
+    portfolioItems[k].classList.add("show");
+}
+else{
+    portfolioItems[k].classList.remove("show");
+    portfolioItems[k].classList.add("hide");
+}
+if(filterValue ==="all"){ 
+    portfolioItems[k].classList.remove("hide");
+    portfolioItems[k].classList.add("show");
+}
+}
+})
+}
+ //portfolio lightbox
+const lightbox=document.querySelector(".lightbox "),
+lightboxImg=lightbox.querySelector(".lightbox-img"),
+lightboxClose=lightbox.querySelector(".lightbox-close")
+lighboxText=lightbox.querySelector(".caption-text"),
+lightboxCounter=lightbox.querySelector(".caption-counter");
+let itemIndex=0;
+
+for(let i=0; i<totalPortfolioItem; i++){
+portfolioItems[i].addEventListener("click", function(){
+       itemIndex=i;
+       changeItem();
+       toogleLightbox();
+      
+})
+}
+function nextItem(){
+    if(itemIndex === totalPortfolioItem-1){
+        itemIndex=0;
+    }
+    else{
+        itemIndex++
+    }
+    changeItem();
+}
+
+function prevItem(){
+    if(itemIndex === 0){
+        itemIndex=totalPortfolioItem-1;
+    }
+    else{
+        itemIndex--;
+    }
+    changeItem();
+}
+//body
+function toogleLightbox(){
+    lightbox.classList.toggle("open");
+
+}
+
+function changeItem(){ 
+imgSrc=portfolioItems[itemIndex].querySelector(".portfolio-img img").getAttribute("src");
+lightboxImg.src=imgSrc;
+lighboxText.innerHTML=portfolioItems[itemIndex].querySelector("h4").innerHTML;
+lightboxCounter.innerHTML=(itemIndex+1) + " of " + totalPortfolioItem;
+
+}
+
+//close lightbox
+lightbox.addEventListener("click",function(event){
+    if(event.target === lightboxClose || event.target === lightbox){
+        toogleLightbox();
+   }
+  
+
+
+})
